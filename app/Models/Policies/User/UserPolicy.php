349a65d353f4142a672faab2a1e61fa8e9f84d84
fhivemind <hivemindf@gmail.com>
2020-11-05 14:12:33 +0100

    api: add user related objects

---
+++ app/Models/Policies/User/UserPolicy.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Models\Policies;
+
+use App\Models\User;
+use App\Models\Role;
+
+class UserPolicy extends BasePolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return mixed
+     */
+    public function viewAll(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\User  $model
+     * @return mixed
+     */
+    public function view(User $user, User $model)
+    {
+        if($user->id === $model->id) {
+            return true;
+        }
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\User  $model
+     * @return mixed
+     */
+    public function update(User $user, User $model)
+    {
+        if($user->id === $model->id) {
+            return true;
+        }
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\User  $model
+     * @return mixed
+     */
+    public function delete(User $user, User $model)
+    {
+        //
+    }
+}
